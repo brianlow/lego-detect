b077ab77e903affaaad98c04d1d5404dd46fe5c7
Brian Low <github@brianlow.com>
2023-04-11 21:15:53 -0600

    Larger run

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 import cv2
 from PIL import Image
 
-model = YOLO('runs/detect/detect-03-50images-hue-shift4/weights/best.pt')
+model = YOLO('runs/detect/detect-04-10-classes-2k-images2/weights/best.pt')
 
 # Predict with the model (grid of lego)
 img = Image.open('/Users/brian/Downloads/lego-datasets/lego_grid.jpeg')
@@ -12,6 +12,6 @@ results = model(img)
 cv2.imwrite("tmp/lego_grid_prediction.png", results[0].plot())
 
 # Predict with the model (one lego)
-img = Image.open('../lego-rendering/renders/dataset/train/images/3001_2.png')
+img = Image.open('../lego-rendering/renders/dataset/train/images/60592_42.png')
 results = model(img)
 cv2.imwrite("tmp/3001_2_prediction.png", results[0].plot())
--- train.py
+++ train.py
@@ -6,8 +6,9 @@
 # !pip uninstall --yes protobuf tensorboard tensorflow
 # !pip install ultralytics protobuf onnx comet_ml
 
-# import comet_ml
-# comet_ml.init(project_name='yolov8')
+import comet_ml
+comet_ml.init(project_name='detect-04-10-classes-2k-images')
+
 
 import sys
 sys.path.append('../ultralytics')
@@ -17,4 +18,4 @@ from ultralytics import YOLO
 model = YOLO('yolov8m.pt')  # load a pretrained model (recommended for training)
 
 # Train the model
-model.train(data=f"../lego-rendering/renders/dataset.yaml", name="detect-03-50images-hue-shift", epochs=30, close_mosaic=7, imgsz=244, hsv_h=1.0, shear=0)
+model.train(data=f"../lego-rendering/renders/dataset.yaml", name="detect-04-10-classes-2k-images", epochs=300, close_mosaic=10, imgsz=244, hsv_h=0.25, shear=0)
