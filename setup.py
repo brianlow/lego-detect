a6deef0303286f3f03dbbc9ae50e6b99c9ea4d69
Brian Low <github@brianlow.com>
2023-04-22 16:13:42 -0600

    Train on 2k real photos

--- setup.py
+++ setup.py
@@ -60,25 +60,49 @@ def convert_voc_to_yolo(voc_root, yolo_output, val_split):
         if not os.path.exists(yolo_image_path):
             img_path.rename(yolo_image_path)
 
+# Start of script
+
+# 80% train, 20% val
+val_split = 0.2
+
+# Make the train/val split repeatable for easier debugging
 random.seed(1)
 
+# Determine where the data will be stored. Either
+#  ./data   - when running locally
+#  /storage - when running on Paperspace
 is_paperspace = os.environ.get('PAPERSPACE_CLUSTER_ID') is not None
 data_dir = Path('/storage' if is_paperspace else './data')
 if not os.path.exists(data_dir):
   os.makedirs(data_dir)
 
+# Download the dataset, have to do this manually right now!
 # from https://mostwiedzy.pl/en/open-research-data/tagged-images-with-lego-bricks,209111650250426-0
 # download https://mostwiedzy.pl/en/open-research-data/tagged-images-with-lego-bricks,209111650250426-0/download to a zip
+
+# Unzip the dataset
 with zipfile.ZipFile(data_dir / 'final_dataset_lego_detection.zip', 'r') as zip_ref:
     zip_ref.extractall(data_dir)
 
-voc_path = os.path.join(data_dir, "final_dataset_lego_detection/photos/10")
-yolo_path = os.path.join(data_dir, "dataset")
-val_split = 0.2  # 80% train, 20% val
-convert_voc_to_yolo(voc_path, yolo_path, val_split)
+# Convert the dataset to YOLO format
+voc_paths = [
+    os.path.join(data_dir, "final_dataset_lego_detection/photos/1"),
+    os.path.join(data_dir, "final_dataset_lego_detection/photos/2"),
+    os.path.join(data_dir, "final_dataset_lego_detection/photos/3"),
+    os.path.join(data_dir, "final_dataset_lego_detection/photos/4"),
+    os.path.join(data_dir, "final_dataset_lego_detection/photos/5"),
+    os.path.join(data_dir, "final_dataset_lego_detection/photos/6"),
+    os.path.join(data_dir, "final_dataset_lego_detection/photos/7"),
+    os.path.join(data_dir, "final_dataset_lego_detection/photos/8"),
+    os.path.join(data_dir, "final_dataset_lego_detection/photos/9"),
+    os.path.join(data_dir, "final_dataset_lego_detection/photos/10"),
+]
+for voc_path in voc_paths:
+    yolo_path = os.path.join(data_dir, "dataset")
+    convert_voc_to_yolo(voc_path, yolo_path, val_split)
 
 # Output a dataset yaml file
-with open(data_dir / 'dataset.yml', 'w') as f:
+with open(data_dir / 'dataset.yaml', 'w') as f:
   f.write(f"# Path must be an absolute path unless it is Ultralytics standard location\n")
   f.write(f"path: {os.path.abspath(data_dir / 'dataset')}\n")
   f.write(f"train: train/images\n")
--- train.py
+++ train.py
@@ -1,3 +1,9 @@
+import os
+import comet_ml
+import sys
+from ultralytics import YOLO
+from pathlib import Path
+
 #
 # Yolo v8 training script
 #
@@ -8,15 +14,18 @@
 
 experiment_name = "detect-05-sample-real"
 
-import comet_ml
 comet_ml.init(project_name=experiment_name)
 
-import sys
-# sys.path.append('../ultralytics')
-from ultralytics import YOLO
+# Determine where the data will be stored. Either
+#  ./data   - when running locally
+#  /storage - when running on Paperspace
+is_paperspace = os.environ.get('PAPERSPACE_CLUSTER_ID') is not None
+data_dir = Path('/storage' if is_paperspace else './data')
+if not os.path.exists(data_dir):
+  os.makedirs(data_dir)
 
 # Load a model
 model = YOLO('yolov8m.pt')  # load a pretrained model (recommended for training)
 
 # Train the model
-model.train(data=f"./data/dataset.yml", name=experiment_name, epochs=300, hsv_h=0.25, shear=0)
+model.train(data=f"./data/dataset.yaml", name=experiment_name, epochs=300, hsv_h=0.25, shear=0)
