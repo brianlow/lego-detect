d81d56d04157f94c246562b247da49b46a39338c
Brian Low <github@brianlow.com>
2023-04-22 00:21:14 -0600

    Setup with final_dataset_lego_detection

--- README.md
+++ README.md
@@ -1,52 +1,9 @@
-Training and sample webpage for recognizing brick
+Training a detection model for identifying lego pieces on a conveyor
 
 
 ## Setup
 
 ```
-install miniconda but don't use the conda envs
-
-pip install torch torchvision
-
-# https://pytorch-lightning.readthedocs.io/en/stable/starter/installation.html
-export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
-export GRPC_PYTHON_BUILD_SYSTEM_ZLIB=1
-python -m pip install -U lightning
-
-https://lightning-flash.readthedocs.io/en/stable/installation.html
-pip install lightning-flash
-pip install 'lightning-flash[image]'
-pip install 'lightning-flash[serve]'
-pip install flask
+asdf install
+python -m pip install -r requirements.txt
 ```
-
-What didn't work
-* asdf
-* conda envs
-
-## Training
-
-* Dataset
-  * 477 lego parts
-  * https://mostwiedzy.pl/en/open-research-data/lego-bricks-for-training-classification-network,618104539639776-0
-* Data prep:
-  * python ./data-prep/square.py
-  * python ./data-prep/extract-subset.py
-* Current best classification model
-  * Yolo v8, xl size
-  * trained on 50 classes, ~96% accurate
-  * Paperspace.com, $8 plan, NVidia A4000 free gpu
-  * Yolo notebook: https://console.paperspace.com/brian22/notebook/r5qgvpedkjbw78v
-
-
-## Running
-
-```
-python serve.py
-./ngrok http 8000
-```
-
-
-## Other
-
-Use ./download_bricks.sh to pull icons for each brick type and save to ./static/images
---
+++ setup.py
@@ -0,0 +1,85 @@
+import os
+import xml.etree.ElementTree as ET
+from pathlib import Path
+import random
+import zipfile
+
+def convert_voc_to_yolo(voc_root, yolo_output, val_split):
+    # Create output directories
+    yolo_output = Path(yolo_output)
+    (yolo_output / 'train' / 'images').mkdir(parents=True, exist_ok=True)
+    (yolo_output / 'train' / 'labels').mkdir(parents=True, exist_ok=True)
+    (yolo_output / 'val' / 'images').mkdir(parents=True, exist_ok=True)
+    (yolo_output / 'val' / 'labels').mkdir(parents=True, exist_ok=True)
+
+    annotations_dir = Path(voc_root)
+    images_dir = Path(voc_root)
+
+    for xml_file in annotations_dir.glob('*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+
+        image_filename = root.find('filename').text
+        img_path = images_dir / image_filename
+        img_name = img_path.stem
+
+        if not os.path.exists(img_path):
+            print(f"#{xml_file} points to a non-existing image: {img_path}")
+            continue
+
+        yolo_annotation = []
+        for obj in root.findall('object'):
+            class_idx = 0 # single class
+
+            bndbox = obj.find('bndbox')
+            xmin = int(bndbox.find('xmin').text)
+            ymin = int(bndbox.find('ymin').text)
+            xmax = int(bndbox.find('xmax').text)
+            ymax = int(bndbox.find('ymax').text)
+
+            width = int(root.find('size').find('width').text)
+            height = int(root.find('size').find('height').text)
+
+            x_center = (xmin + xmax) / 2 / width
+            y_center = (ymin + ymax) / 2 / height
+            box_width = (xmax - xmin) / width
+            box_height = (ymax - ymin) / height
+
+            yolo_annotation.append(f"{class_idx} {x_center} {y_center} {box_width} {box_height}")
+
+        train_or_val = 'val' if random.random() < val_split else 'train'
+
+        # Save YOLO annotation file
+        yolo_label_path = yolo_output / train_or_val / 'labels' / f"{img_name}.txt"
+        if not os.path.exists(yolo_label_path):
+            with open(yolo_label_path, 'w') as f:
+                f.write('\n'.join(yolo_annotation))
+
+        # Copy the image to the YOLO output directory
+        yolo_image_path = yolo_output / train_or_val / 'images' / img_path.name
+        if not os.path.exists(yolo_image_path):
+            img_path.rename(yolo_image_path)
+
+random.seed(1)
+
+is_paperspace = os.environ.get('PAPERSPACE_CLUSTER_ID') is not None
+data_dir = Path('/storage' if is_paperspace else './data')
+if not os.path.exists(data_dir):
+  os.makedirs(data_dir)
+
+# download https://mostwiedzy.pl/en/open-research-data/tagged-images-with-lego-bricks,209111650250426-0/download to a zip
+with zipfile.ZipFile(data_dir / 'final_dataset_lego_detection.zip', 'r') as zip_ref:
+    zip_ref.extractall(data_dir)
+
+voc_path = os.path.join(data_dir, "final_dataset_lego_detection/photos/10")
+yolo_path = os.path.join(data_dir, "dataset")
+val_split = 0.2  # 80% train, 20% val
+convert_voc_to_yolo(voc_path, yolo_path, val_split)
+
+# Output a dataset yaml file
+with open(data_dir / 'dataset.yml', 'w') as f:
+  f.write(f"train: train/images\n")
+  f.write(f"val: val/images\n")
+  f.write(f"\n")
+  f.write(f"names:\n")
+  f.write(f"  0: lego\n")
